93a11609e4c17104d3ba1066bcfcbcb517c9bbd3
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-09 12:19:16 +0000

    Reset to a2913f0c9ac2e7260c0e7f6dfbea6a9380be996a and made changes for LLM Round 1

--- src/drd/prompts/instructions.py
+++ src/drd/prompts/instructions.py
@@ -3,31 +3,14 @@
   <steps>
     <step>
       <type>shell</type>
-      <command>command to execute</command>
-    </step>
-    <step>
-      <type>file</type>
-      <operation>CREATE</operation>
-      <filename>path/to/file.ext</filename>
-      <content>
-        <![CDATA[
-          def example():
-           re...
-        ]]> 
-      </content>
+      <command>npx create-next-app@latest .</command>
     </step>
     <step>
       <type>file</type>
       <operation>UPDATE</operation>
-      <filename>path/to/existing/file.ext</filename>
+      <filename>app.py</filename>
       <content>
         <![CDATA[
-          Specify changes using the following format:
-          + line_number: content to add
-          - line_number: (to remove the line)
-          r line_number: content to replace the line with
-          
-          Example:
           + 3: import new_module
           - 10:
           r 15: def updated_function():
@@ -35,38 +18,22 @@
       </content>
     </step>
     <step>
-      <type>file</type>
-      <operation>DELETE</operation>
-      <filename>path/to/file/to/delete.ext</filename>
-    </step>
-    <step>
       <type>metadata</type>
       <operation>UPDATE_FILE</operation>
       <filename>drd.json</filename>
       <content>
         <![CDATA[
-          {
-            "project_name": "pyser",
-            "files": [
-              {
-                "filename": "app.py",
-                "type": "Python",
-                "description": "...',
-                "exports": "None"
-              },
-              {
-                "filename": "drd.json",
-                "type": "json",
-                "description": "",
-                "exports": "None"
-              }
-            ],
-            "dev_server": {
-              "start_command": "python start",
-              "framework": "flask",
-              "language": "python"
-            }
-          }
+          {}
+        ]]> 
+      </content>
+    </step>
+    <step>
+      <type>metadata</type>
+      <operation>UPDATE</operation>
+      <filename>drd.json</filename>
+      <content>
+        <![CDATA[
+          {}
         ]]> 
       </content>
     </step>
